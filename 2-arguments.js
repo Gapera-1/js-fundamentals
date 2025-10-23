d28bdc7d81861d29d8e38385e9d0ce87ed14bfc8
Francois <fmanishimwe38@gmail.com>
2025-10-23 10:55:52 +0200

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,10 +3,11 @@ const args = process.argv.slice(2);
 if (args.length===0){
     console.log("No argument");
 }
-else if (args===1){
+else if (args.length===1){
     console.log("Argument found");
 }
 else{
     console.log("Arguments found");
 
 }
+
