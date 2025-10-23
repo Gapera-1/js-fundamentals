d663f46bbc9fbece7cfdca9d2f31899666f0f49a
Francois <fmanishimwe38@gmail.com>
2025-10-23 10:49:52 +0200

    Add files via upload

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+
+const args = Process.argv.slice(2);
+if (args.length===0){
+    console.log("No argument");
+}
+else if (args===1){
+    console.log("Argument found");
+}
+else{
+    console.log("Arguments found");
+}
